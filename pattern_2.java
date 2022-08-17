e452ac6eab516a99cf65f5582fd9cdc608be4050
Snigdha Gupta <80680269+snigdha2808@users.noreply.github.com>
2022-08-18 01:02:18 +0530

    Create pattern_2.java

---
+++ pattern_2.java
@@ -0,0 +1,31 @@
+/*
+       1 
+       2 3 
+       4 5 6 
+       7 8 9 10 
+       11 12 13 14 15 
+ */
+package super_150;
+import java.util.*;
+public class pattern_2 {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int num=1;
+		for(int i=1;i<=n;i++)
+		{
+			
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(num+ " ");
+				num++;
+			}
+			System.out.println();
+		}
+
+	}
+
+}
