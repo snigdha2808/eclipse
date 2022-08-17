026275422272e265822f27761ae273a7fc17d79e
Snigdha Gupta <80680269+snigdha2808@users.noreply.github.com>
2022-08-17 20:13:34 +0530

    Create CodingBlocks3446.java

---
+++ CodingBlocks3446.java
@@ -0,0 +1,21 @@
+package super_150;
+import java.util.*;
+public class CodingBlocks3446 {
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int sum=0;
+		int pos=1;
+		while(n>0)
+		{
+			int rem=n%10;
+			sum=sum+pos*(int)Math.pow(10, rem-1);
+			n/=10;
+			pos++;
+			
+		}
+		System.out.println(sum);
+	}
+
+}
