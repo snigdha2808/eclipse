0e0bf00b12e82992745e2d014d0e61ed8f504468
Snigdha Gupta <80680269+snigdha2808@users.noreply.github.com>
2022-08-18 00:26:42 +0530

    Create pateern_4.java

---
+++ pateern_4.java
@@ -0,0 +1,27 @@
+package super_150;
+import java.util.*;
+public class pattern_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int r=sc.nextInt();
+		int col=sc.nextInt();
+		for(int i=1;i<=r;i++)
+		{
+			for(int j=1;j<=col;j++)
+			{
+				if(i==1 || j==1 || i==r || j==col)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+
+}
