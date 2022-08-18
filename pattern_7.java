fa7a86549b79fee1f4e42276645b9314ed3f8be7
Snigdha Gupta <80680269+snigdha2808@users.noreply.github.com>
2022-08-18 11:02:02 +0530

    Create pattern_7.java

---
+++ pattern_7.java
@@ -0,0 +1,32 @@
+/*
+
+    *****
+   *****
+  *****
+ *****
+*****
+
+*/
+package super_150;
+import java.util.*;
+public class Pattern_7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=1;i<=n;i++)
+		{
+			for(int k=n-i;k>=1;k--)
+			{
+				System.out.print(" ");
+			}
+			for(int j=1;j<=n;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+}
