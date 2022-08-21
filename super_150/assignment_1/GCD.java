c599a26b2fd8c67907f988a15d907f9197165358
Snigdha Gupta <80680269+snigdha2808@users.noreply.github.com>
2022-08-21 22:36:03 +0530

    Create GCD.java

---
+++ super_150/assignment_1/GCD.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main {
+		public static int calculateGcd(int a,int b)
+	{
+		int res=Math.min(a,b);
+		while(res>0)
+		{
+			if(a%res==0 && b%res==0)
+			{
+				break;
+			}
+			res--;
+		}
+		return res;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int res=calculateGcd(a,b);
+		System.out.println(res);
+	}
+}
