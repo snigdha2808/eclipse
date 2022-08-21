b23b452a16f99203c05842432c7ad9575da7b4b8
Snigdha Gupta <80680269+snigdha2808@users.noreply.github.com>
2022-08-21 22:38:12 +0530

    Create check_prime.java

---
+++ super_150/assignment_1/check_prime.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Main {
+    public static void main(String args[]) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		boolean flag = false;
+    	for (int i = 2; i <= n/ 2; i++) 
+		{
+      		if (n% i == 0) 
+			{
+        		flag = true;
+        		break;
+      		}
+    	}
+		if (!flag)
+		{
+      		System.out.println("Prime");
+		}
+		else
+		{
+      		System.out.println("Not Prime");
+		}
+  	}
+}
